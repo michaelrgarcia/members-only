81f3372bdf673d4b32d1d94086829df8e9b4b11f
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-02-26 04:37:56 -0800

    Finish schema

--- db/populateDb.ts
+++ db/populateDb.ts
@@ -4,7 +4,24 @@ import { config } from "dotenv";
 
 config();
 
-const SQL = `database schema here`;
+const SQL = `
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  first_name VARCHAR ( 50 ),
+  last_name VARCHAR ( 50 ),
+  email VARCHAR ( 255 ),
+  password TEXT,
+  admin BOOLEAN
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR ( 90 ),
+  content VARCHAR ( 500 ),
+  author INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  created TIMESTAMPTZ
+);
+`;
 
 // this script should be ran ONCE with npx tsx populateDb.ts
 
