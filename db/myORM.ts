ae09a48a6c103aa79579a77eba5fdae0404f218f
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-02-27 17:27:53 -0800

    Create insert / select functions

--- db/myORM.ts
+++ db/myORM.ts
@@ -5,7 +5,15 @@ interface WhereCondition {
   equals: any;
 }
 
-export async function rowSelect(table: string, condition?: WhereCondition) {
+interface Item {
+  column: string;
+  value: any;
+}
+
+export async function rowSelect(
+  table: string,
+  condition?: WhereCondition
+): Promise<any> {
   // add support for "AND" conditions? WhereCondition[]
 
   if (condition) {
@@ -22,4 +30,17 @@ export async function rowSelect(table: string, condition?: WhereCondition) {
 
     return rows;
   }
+}
+
+export async function tableInsert(table: string, items: Item[]) {
+  const columns = items.map((item) => item.column).join(", ");
+  const parameters = items
+    .map((_, itemIndex) => "$" + `${itemIndex + 1}`)
+    .join(", ");
+  const values = items.map((item) => item.value);
+
+  return await pool.query(
+    `INSERT INTO ${table} (${columns}) VALUES (${parameters})`,
+    values
+  );
 }
